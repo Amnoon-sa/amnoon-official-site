085d10db30843c239efc7d1f47294b5d3269a741
rayansb1 <rayanbawazeer1@hotmail.com>
2026-01-14 19:49:53 +0300

    Update company name and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ const Header = ({ lang }) => {
       {lang === 'ar' ? (
         <>
           <img src="/img/main/2030logo.png" alt="رؤية السعودية 2030" style={{ height: '120px' }} />
-          <h1>آمنون للإستشارات الأمنية</h1>
+          <h1>ﺷﺮﻛﺔ اﻣﻨﻮن ﻟﻼﺳﺘﺸﺎرات اﻟﻤﻬﻨﻴﺔ</h1>
           <img src="/img/main/logo-white.png" alt="شعار الشركة" style={{ width: '255px', height: '250px' }} />
         </>
       ) : (
         <>
-          <img src="/img/main/logo-white.png" alt="Company Logo" style={{ width: '255px', height: '250px' }} />
-          <h1>Amnoon for Security Consultant</h1>
+          <img src="/img/main/logo-white.png" alt="Company Logo" style={{ width: '140px', height: '150px' }} />
+          <h1>Amnoon Professional Consulting Compan</h1>
           <img src="/img/main/2030logo.png" alt="Saudi 2030 Vision Logo" style={{ height: '120px' }} />
         </>
       )}
